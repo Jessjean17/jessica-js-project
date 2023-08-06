03a78bcefaf1c5b612398a76e5190f821723276d
Jessjean17 <140281920+Jessjean17@users.noreply.github.com>
2023-08-06 19:37:57 +0000

    done with project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function myFunction(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+let result = `${who[myFunction(who)]}
+  ${action[myFunction(action)]}
+    ${what[myFunction(what)]} 
+    ${when[myFunction(when)]}`;
+document.getElementById("excuse").innerHTML = result;
